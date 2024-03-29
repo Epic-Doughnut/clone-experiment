916218a4c1f21da1d0f83d94f0ddeed531b0149b
Joey Sherrill <joeysherrill1387@gmail.com>
2024-03-29 01:42:20 -0600

    "Added globalRates object to window scope in currentRates.js"
    This commit updates the scripts/json/currentRates.js file by adding a line to make the 'globalRates' object visible in the console through the window object (i.e., window.globalRates = globalRates;). The commit also initializes the 'globalRates' object by setting its keys to the keys of the 'allMaterials' object and initializing their values to 0.

--- scripts/json/currentRates.js
+++ scripts/json/currentRates.js
@@ -1,6 +1,8 @@
 const { allMaterials } = require('./allMaterials');
 
 var globalRates = {};
+// Make visible in console
+window.globalRates = globalRates;
 // Initialize keys
 Object.keys(allMaterials).forEach((key) => {globalRates[key] = 0;});
 
